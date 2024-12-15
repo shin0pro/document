57df688594dbfd91504e4e8e7d40f8d364b1f981
shin0pro <ngukhong00@gmail.com>
2024-12-15 20:19:27 +0700

    Update search-results.js

--- js/search-results.js
+++ js/search-results.js
@@ -11,6 +11,13 @@ const searchData = [
     { title: "Tài liệu HTML cơ bản", link: "tailieuhtmlcoban.html" },
     { title: "Tài liệu C++ cơ bản", link: "tailieuc++coban.html" },
     { title: "Tài liệu Toán CNTT", link: "tl_toan_cntt.html" },
+    { title: "Tài liệu Java Script", link: "tl_jv.html" },
+    { title: "Tài liệu Tiếng Anh chuyên ngành CNTT", link: "tl_chuyen_nganh_cntt.html" },
+    { title: "Tài liệu một số môn khác", link: "khác.html" },
+    { title: "Các kiến thức thêm về CNTT", link: "khampha.html" },
+    { title: "Bảng so sánh các ngôn ngữ lập trình hiện nay", link: "bảng.html" },
+    { title: "Video về sự phát triển của khoa học ngày nay", link: "mp4.html" },
+    { title: "upload", link: "upload.html" },
 ];
 
 // Lọc kết quả tìm kiếm dựa trên từ khóa
